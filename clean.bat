c7cb7e46b9c7b278c135e667386b3305ae8ac068
miyajan <miyajan777@gmail.com>
2017-01-15 19:51:06 +0900

    double quotation

--- clean.bat
+++ clean.bat
@@ -1,5 +1,2 @@
-cd %~pd0
-rd /s /q %~pd0target %~pd0WEB-INF\classes
-mkdir %~pd0target\classes
-dir /s /B *.java > %~pd0target\sources.txt
-javac -cp "%~pd0lib\*" -d %~pd0/target\classes @sources.txt
+cd "%~pd0"
+rd /s /q "%~pd0target" "%~pd0WEB-INF\classes"
--- compile.bat
+++ compile.bat
@@ -1,4 +1,4 @@
-cd %~pd0
-mkdir %~pd0target\classes
-dir /s /B *.java > %~pd0target\sources.txt
-javac -cp "%~pd0lib\*" -d %~pd0target\classes @sources.txt
+cd "%~pd0"
+mkdir "%~pd0target\classes"
+dir /s /B *.java > "%~pd0target\sources.txt"
+javac -cp "%~pd0lib\*" -d "%~pd0target\classes" "@%~pd0target\sources.txt"
