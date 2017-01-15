5af0ab443da9060e9abefc9876fa878cb262bb06
miyajan <miyajan777@gmail.com>
2017-01-15 20:32:19 +0900

    not use sources.txt

--- compile.bat
+++ compile.bat
@@ -1,4 +1,3 @@
 cd "%~pd0"
 mkdir target\classes
-forfiles /s /m *.java /c "cmd /c echo @relpath" > target\sources.txt
-javac -cp "lib\*" -d "target\classes" @target\sources.txt
+javac -cp "lib\*" -d "target\classes" src/main/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/*.java src/test/java/schoo/jenkins/runner/*.java
