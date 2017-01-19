75aa1e0f390fb8b5737954ffaaf5603295241aaa
Miyata Jumpei <miyajan777@gmail.com>
2017-01-19 20:27:32 +0900

    Update FizzBuzz.java

--- src/main/java/schoo/jenkins/FizzBuzz.java
+++ src/main/java/schoo/jenkins/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         if (value % 15 == 0) {
             return "FizzBuzz";
         } else if (value % 3 == 0) {
-            return "Fuzz";
+            return "Fizz";
         } else if (value % 5 == 0) {
             return "Buzz";
         } else {
