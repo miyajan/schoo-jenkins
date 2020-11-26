97857b84867eee223e1c04766dbc01762aa88b6c
ozawa-n <75060009+ozawa-n@users.noreply.github.com>
2020-11-26 15:06:18 +0900

    Update FizzBuzz.java

--- src/main/java/schoo/jenkins/FizzBuzz.java
+++ src/main/java/schoo/jenkins/FizzBuzz.java
@@ -5,7 +5,7 @@ public class FizzBuzz {
         if (value % 15 == 0) {
             return "FizzBuzz";
         } else if (value % 3 == 0) {
-            return "Fizz";
+            return "Fuzz";
         } else if (value % 5 == 0) {
             return "Buzz";
         } else {
